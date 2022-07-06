afb511986c43f57215f48bab8cdcab78244ea3e0
Caprese17 <84934222+Caprese17@users.noreply.github.com>
2022-07-06 14:13:18 -0400

    Add files via upload

---
+++ Assignment 3.ps1
@@ -0,0 +1,56 @@
+﻿
+$total = 0
+$n = 0
+$avg = 0.0
+$min = 999
+$max = 0
+$file = ""
+$fileexists = ""
+
+do {
+    $num = Read-Host " Enter a number between 0 and 999"
+    Write-Host "You Entered: " $num
+    if($num -lt 0){ 
+       Write-Host "You Entered An Invalid Number " $num
+       }
+    else{ 
+       $total= $total + $num 
+       $n++
+       If($num -lt $min){
+          $min = $num
+       }
+       If($num -gt $max -and $num -ne 999){
+          $max = $num
+       }
+}} until ($num -eq 999)
+
+$avg = ($total - 999)/($n - 1)
+Write-Host "Average: " $avg 
+
+Write-Host "The smallest number is: " $min
+
+Write-Host "The largest number is: " $max
+
+$file = Read-Host "Please Enter a File Name: "
+
+
+$fileexists = Test-Path -Path $file
+
+        If($fileexists) {
+        Write-Host "File Already Exists"
+           
+        }
+        else { 
+        New-Item -Path . -Name $file -ItemType "file" -Value "Current Date                  Average       Lowest     Highest `r `n"
+  
+        Add-Content -Path $file -Value "Monday, Nov. 29 2021             $avg           $min          $max"
+        }
+
+
+
+
+
+
+ 
+  
+
---
+++ KnockKnock.ps1
@@ -0,0 +1,95 @@
+Clear-Host
+
+$userReply = ""
+
+while ($userReply -ne "Who is there?"){
+   $userReply = read-host "Knock Knock!"
+}
+
+Clear-Host
+while ($userReply -ne "Figs who?") {
+   $userReply = read-host "Figs."
+}
+
+Clear-Host
+Write-Output "Figs the doorbell, it's not working!"
+
+Start-Sleep -Seconds 5
+
+Clear-Host
+
+while($userReply -ne "Who is there?"){
+   $userReply = read-host "Knock Knock!"
+}
+
+Clear-Host
+while ($userReply -ne "Luke who?"){
+   $userReply = read-host "Luke."
+}
+
+Clear-Host
+Write-Output "Luke through the peep hole and find out."
+
+Start-Sleep -Seconds 5
+
+Clear-Host
+
+while ($userRely -ne "Who is there?"){
+   $userReply = read-host "Knock Knock!"
+}
+
+Clear-Host
+
+while ($userReply -ne "Alice who?"){
+   $userReply = read-host "Alice."
+}
+
+Clear-Host
+Write-Ouput "Alice fair in love and war."
+
+Start-Sleep -Second 5
+
+Clear-Host
+
+while ($userRely -ne "Who is there?"){
+   $userReply = read-host "Knock Knock!"
+}
+
+Clear-Host
+
+while ($userReply -ne "Snow who?"){
+   $userReply = read-host "Snow."
+}
+
+Clear-Host
+Write-Ouput "Snow use. The joke is over."
+
+Start-Sleep -Second 5
+
+Clear-Host
+
+while ($userRely -ne "Who is there?"){
+   $userReply = read-host "Knock Knock!"
+}
+
+Clear-Host
+
+while ($userReply -ne "Woo who?"){
+   $userReply = read-host "Woo."
+}
+
+Clear-Host
+Write-Ouput "Glad you're excited, too."
+
+Start-Sleep -Second 5
+
+Clear-Host
+
+Write-Output "The Knock Knock Joke"
+Write-Output ""
+Write-Output "Copyright 2021 - Caprese Smith"
+
+Start-Sleep -Seconds 3
+
+Clear-Host
+ 
